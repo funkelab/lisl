refactor evaluation, fix rotation augmentation bug, add output files for later analysis

lisl/pl/evaluation.py
@@ -248,8 +248,8 @@ def n(a):
 
 class AnchorSegmentationValidation(Callback):
 
-    def __init__(self, run_ms_segmentation=True, device='cpu'):
-        self.run_ms_segmentation = run_ms_segmentation
+    def __init__(self, run_segmentation=False, device='cpu'):
+        self.run_segmentation = run_segmentation
         self.device = device
 
         super().__init__()
@@ -264,7 +264,7 @@ class AnchorSegmentationValidation(Callback):
             print("seg_score_best_bandwidth", np.mean(self.seg_scores[k]))
             pl_module.log(f"{k}_seg_score", np.mean(self.seg_scores[k]))
 
-    def predict_embedding(self, batch, pl_module, patch_size):
+    def predict_embedding(self, batch, pl_module, patch_size, augmentation):
 
         edim = pl_module.out_channels
 
@@ -290,7 +290,7 @@ class AnchorSegmentationValidation(Callback):
                 b, p, c, pw, ph = patches.shape
 
                 patches = patches.cuda()
-                pred_i = pl_module.forward_patches(patches)
+                pred_i = pl_module.forward_patches(patches, augmentation=augmentation)
                 pred_i = pred_i.to(embedding.device)
 
                 embedding_relative[:, :, i] = pred_i.permute(0, 2, 1).view(b, edim, x.shape[-1])
@@ -300,11 +300,12 @@ class AnchorSegmentationValidation(Callback):
 
         return embedding, embedding_relative
 
-    def create_eval_dir(self, pl_module):
+    def create_eval_dir(self, pl_module, augmentation):
         eval_directory = os.path.abspath(os.path.join(pl_module.logger.log_dir,
                                                       os.pardir,
                                                       os.pardir,
                                                       "evaluation",
+                                                      augmentation,
                                                       f"{pl_module.global_step:08d}"))
 
         os.makedirs(eval_directory, exist_ok=True)
@@ -312,7 +313,8 @@ class AnchorSegmentationValidation(Callback):
 
 
     def visualize_embeddings(self, embedding, x, filename):
-        for b, e in enumerate(embedding.cpu().numpy()):
+        for b in range(len(embedding)):
+            e = embedding[b].cpu().numpy()
             for c in range(0, embedding.shape[1], 2):
                 imsave(f"{filename}_{b}_{c}.jpg", 
                        np.stack((n(e[c]), n(x[b, 0].cpu().numpy()), n(e[c+1])), axis=-1))
@@ -326,7 +328,8 @@ class AnchorSegmentationValidation(Callback):
 
     def visualize_embedding_vectors(self, embedding_relative, x, filename, downsample_factor=8):
 
-        for b, e in enumerate(embedding_relative):
+        for b in range(len(embedding_relative)):
+            e = embedding_relative[b].cpu().numpy()
 
             cx = np.arange(e.shape[-2], dtype=np.float32)
             cy = np.arange(e.shape[-1], dtype=np.float32)
@@ -346,61 +349,39 @@ class AnchorSegmentationValidation(Callback):
                                  grad=None,
                                  output_file=f"{filename}_{b}.jpg")
 
-
-    def on_validation_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
-
-        x, patches, abs_coords, patch_matches, mask, y = batch
-
-        eval_directory = self.create_eval_dir(pl_module)
-        embedding, embedding_relative = self.predict_embedding(batch, pl_module, trainer.datamodule.patch_size)
-
-        self.visualize_embeddings(embedding, x, f"{eval_directory}/embedding_{batch_idx}_{pl_module.local_rank}")
-        self.visualize_embeddings(embedding_relative, x,
-            f"{eval_directory}/embedding_relative_{batch_idx}_{pl_module.local_rank}")
-
-        filename = f"{eval_directory}/pointer_embedding_{batch_idx}_{pl_module.local_rank}"
-        self.visualize_embedding_vectors(embedding_relative, x, filename)
-
-        eval_data_file = f"{eval_directory}/embedding_{batch_idx}_{pl_module.local_rank}.zarr"
-
+    def write_embedding_to_file(self, eval_data_file, embedding, embedding_relative, x, y):
         # write embedding and raw data to file
         z_array = zarr.open(eval_data_file, mode="w")
-        for b, e in enumerate(embedding_relative.cpu().numpy()):
-            z_array.create_dataset(f"{b}/embedding", data=e, compression='gzip')
+        for b in range(len(embedding_relative)):
+            z_array.create_dataset(f"{b}/embedding", data=embedding_relative[b].cpu().numpy(), compression='gzip')
             z_array.create_dataset(f"{b}/embedding_abs", data=embedding.cpu().numpy()[b], compression='gzip')
             z_array.create_dataset(f"{b}/gt_segmentation", data=y.cpu().numpy()[b, None], compression='gzip')
             z_array.create_dataset(f"{b}/raw", data=x[b].cpu().numpy(), compression='gzip')
             threshold = skimage.filters.threshold_li(image=x[b].cpu().numpy())
             z_array.create_dataset(f"{b}/threshold_li", data=255*(x[b].cpu().numpy() > threshold).astype(dtype=np.uint8), compression='gzip')
 
+        return z_array
 
+    def meanshift_segmentation(self, embedding, bandwidths):
         # Compute Meanshift Segmentation
-        bandwidths = [1, 2, 4] + list(np.arange(5., 30., 10.))
-        seg_score_table = np.zeros((len(bandwidths), len(embedding)))
-
+        ms_segmentaiton = {}
 
         for i, bandwidth in enumerate(bandwidths):
+            ms_segmentaiton[bandwidth] = []
+
             segment_with_meanshift = AnchorMeanshift(bandwidth)
             ms_segmentation = segment_with_meanshift(embedding)
 
             for b, seg in enumerate(ms_segmentation):
+                ms_segmentaiton[bandwidth].append(seg)
 
-                seg_score_table[i, b] = seg_metric(seg, y[0].numpy())
+        return ms_segmentaiton
 
-                z_array.create_dataset(f"{b}/meanshift_seg_{bandwidth}", data=seg[None], compression='gzip')
+    def mws_segmentation(self, embedding, foreground_masks, temperatures, img_dir_and_prefix=None, output_z_array=None):
 
-                self.visualize_segmentation(seg, x[b], 
-                    f'{eval_directory}/meanshift_seg_{batch_idx}_{pl_module.local_rank}_{b}_{bandwidth}.jpg')
-
-        if "meanshift" not in self.seg_scores:
-            self.seg_scores["meanshift"] = []
-        self.seg_scores["meanshift"].append(seg_score_table.max(axis=0).mean())
-        print("meanshift table ", seg_score_table)
-
-        # Compute MWS Segmentation
+        mws_segmentaiton = {}
 
         # compute affinities
-
         att_c = 2
         offsets = np.array([[-1, 0], [0, -1],
                             [-9, 0], [0, -9],
@@ -408,23 +389,28 @@ class AnchorSegmentationValidation(Callback):
                             [-9, -4], [-4, -9], [4, -9], [9, -4],
                             [-27, 0], [0, -27]], int)
 
-        temperatures = [1., 10., 100.]
-        seg_score_table = np.zeros((len(temperatures), len(embedding)))
+        
         for i, temperature in enumerate(temperatures):
             def affinity_measure(x, y, dim=0):
                 distance = (x - y).norm(2, dim=dim)
                 return (-distance.pow(2) / temperature).exp()
 
+            mws_segmentaiton[temperature] = []
+
             for b, emb in enumerate(embedding):
                 affinities = embedding_to_affinities(emb,
                                                      offsets=offsets,
                                                      affinity_measure=affinity_measure)
-                z_array.create_dataset(f"{b}/affinities_{temperature}",
-                                       data=affinities.cpu().numpy(),
-                                       compression='gzip', overwrite=True)
 
-                foreground_mask = (np.array(z_array[f"{b}/threshold_li"]) > 0)[0]
-
+                if img_dir_and_prefix is not None:
+                    img_dir, prefix = img_dir_and_prefix
+                    for c in range(len(affinities)):
+                        imsave(f"{img_dir}/affinity_{prefix}_{b}_{temperature}_{c}.jpg", 
+                               affinities[c].cpu().numpy())
+                if output_z_array is not None:
+                    output_z_array.create_dataset(f"{b}/affinities_{temperature}",
+                                           data=affinities.cpu().numpy(),
+                                           compression='gzip', overwrite=True)
                 # compute affinityies
                 affinities[:, :att_c] *= -1
                 affinities[:, :att_c] += 1
@@ -433,21 +419,90 @@ class AnchorSegmentationValidation(Callback):
                                    offsets,
                                    att_c,
                                    strides=None,
-                                   mask=foreground_mask).astype(np.int32)
-
-                seg_score_table[i, b] = seg_metric(seg, y[0].numpy())
-
-                self.visualize_segmentation(seg, x[b], 
-                    f'{eval_directory}/mws_seg_{batch_idx}_{pl_module.local_rank}_{b}_{temperature}.jpg')
-
-                z_array.create_dataset(f"{b}/mws_seg_{temperature}",
-                    data=label_cont(seg)[None].astype(np.uint32),
-                    compression='gzip', overwrite=True)
+                                   mask=foreground_masks[b]).astype(np.int32)
+                seg = label_cont(seg)
+                mws_segmentaiton[temperature].append(seg)
             
-        if "MutexWS" not in self.seg_scores:
-            self.seg_scores["MutexWS"] = []
-        print("mws table ", seg_score_table)
-        self.seg_scores["MutexWS"].append(seg_score_table.max(axis=0).mean())        
+
+        return mws_segmentaiton
+
+
+
+    def log_SEG_score(self, segmentation_dict, gt_segmentation, evaluation_key=None):
+
+        seg_score_table = []
+        for k in segmentation_dict:
+            seg_scores = []
+            for j, seg in enumerate(segmentation_dict[k]):
+                seg_scores.append(seg_metric(seg, gt_segmentation[j].numpy()))
+            seg_score_table.append(seg_scores)
+
+        seg_score_table = np.array(seg_score_table)
+        SEG_score = seg_score_table.max(axis=0).mean()
+
+        if evaluation_key is not None:
+            if evaluation_key not in self.seg_scores:
+                self.seg_scores[evaluation_key] = []
+            self.seg_scores[evaluation_key].append(SEG_score)
+
+        return SEG_score
+
+    def write_segmentation_to_zarr(self, segmentation_dict, key, z_array):
+        for k in segmentation_dict:
+            for b, seg in enumerate(segmentation_dict[k]):
+                z_array.create_dataset(f"{b}/{key}_{k}", data=label_cont(seg[None]), compression='gzip')
+
+    def visualizalize_segmentation_dict(self, segmentation_dict, x, filename):
+        for k in segmentation_dict:
+            for b, seg in enumerate(segmentation_dict[k]):
+                self.visualize_segmentation(seg, x[b], f'{filename}_{b}_{k}.jpg')
+
+    def full_evaluation(self, trainer, pl_module, batch, batch_idx, dataloader_idx, augmentation):
+
+        x, patches, abs_coords, patch_matches, mask, y = batch
+
+        eval_directory = self.create_eval_dir(pl_module, augmentation)
+
+        trainer.model.eval()
+        embedding, embedding_relative = self.predict_embedding(batch,
+                                                               pl_module,
+                                                               trainer.datamodule.patch_size,
+                                                               augmentation)
+        trainer.model.train()
+
+        vis_pointer_filename = f"{eval_directory}/pointer_embedding_{batch_idx}_{pl_module.local_rank}"
+        vis_embedding_filename = f"{eval_directory}/embedding_{batch_idx}_{pl_module.local_rank}"
+        vis_relembedding_filename = f"{eval_directory}/embedding_relative_{batch_idx}_{pl_module.local_rank}"
+        self.visualize_embedding_vectors(embedding_relative, x, vis_pointer_filename)
+        self.visualize_embeddings(embedding, x, vis_embedding_filename)
+        self.visualize_embeddings(embedding_relative, x, vis_relembedding_filename)
+        
+        eval_data_file = f"{eval_directory}/embedding_{batch_idx}_{pl_module.local_rank}.zarr"
+        z_array = self.write_embedding_to_file(eval_data_file, embedding, embedding_relative, x, y)
+
+        if self.run_segmentation:
+
+            ms_bandwidths = (1, 2, 4) + tuple(np.arange(5., 30., 10.))
+            ms_seg = self.meanshift_segmentation(embedding, ms_bandwidths)
+
+            evaluation_key = f"meanshift_{augmentation}"
+            seg_score = self.log_SEG_score(ms_seg, y, evaluation_key=evaluation_key)
+            self.write_segmentation_to_zarr(ms_seg, "meanshift", z_array)
+            self.visualizalize_segmentation_dict(ms_seg, x, f'{eval_directory}/meanshift_seg_{batch_idx}_{pl_module.local_rank}')
+
+            temperatures = [1., 10., 100.]
+            foreground_mask = [(np.array(z_array[f"{b}/threshold_li"]) > 0)[0] for b in range(len(embedding))]
+
+            mws_seg = self.mws_segmentation(embedding, foreground_mask, temperatures,
+                                            img_dir_and_prefix=(eval_directory, f"{batch_idx}_{pl_module.local_rank}"))
+            evaluation_key = f"MWS_{augmentation}"
+            seg_score = self.log_SEG_score(mws_seg, y, evaluation_key=f"MWS_{augmentation}")
+            self.write_segmentation_to_zarr(mws_seg, "MWS", z_array)
+            self.visualizalize_segmentation_dict(mws_seg, x, f'{eval_directory}/mws_seg_{batch_idx}_{pl_module.local_rank}')
+
+    def on_validation_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        for augmentation in ["noaugmentation", "rotflip"]:
+            self.full_evaluation(trainer, pl_module, batch, batch_idx, dataloader_idx, augmentation)
 
 class AnchorMeanshift():
     def __init__(self, bandwidth, reduction_probability = 0.05):

lisl/pl/train.py
@@ -12,6 +12,7 @@ from lisl.pl.datamodules import MosaicDataModule, SSLDataModule
 from lisl.pl.utils import save_args, import_by_string
 from lisl.pl.evaluation import SupervisedLinearSegmentationValidation, AnchorSegmentationValidation
 from lisl.pl.callbacks import Timing
+from pytorch_lightning.callbacks import LearningRateMonitor
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 from test_tube import HyperOptArgumentParser
@@ -37,18 +38,20 @@ if __name__ == '__main__':
 
     # init module
     model = SSLTrainer.from_argparse_args(args)
+    # model.model.load_state_dict(torch.load(some_file_path))
+
     datamodule = dmodule.from_argparse_args(args)
     # ssl_test_acc = SupervisedLinearSegmentationValidation.from_argparse_args(args)
-    anchor_val = AnchorSegmentationValidation(run_ms_segmentation=True)
-    # lr_logger = LearningRateLogger()
+    anchor_val = AnchorSegmentationValidation(run_segmentation=True)
+    lr_logger = LearningRateMonitor(logging_interval='step')
     timer = Timing()
-    model_saver = ModelCheckpoint(save_last=True, save_weights_only=False, period=10)
+    model_saver = ModelCheckpoint(save_last=True, save_weights_only=False, period=100)
 
     #  init trainer
     trainer = pl.Trainer.from_argparse_args(args)
 
     # trainer.callbacks.append(ssl_test_acc)
     trainer.callbacks.append(anchor_val)
-    # trainer.callbacks.append(lr_logger)
+    trainer.callbacks.append(lr_logger)
     trainer.callbacks.append(timer)
     trainer.fit(model, datamodule)

lisl/pl/trainer.py
@@ -64,6 +64,7 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
                        pretrained_model=False,
                        resnet_size=18,
                        val_patch_inference_steps=None,
+                       val_patch_inference_downsample=None,
                        lr_milestones=(100)):
 
         super().__init__()
@@ -90,6 +91,7 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
 
         self.val_train_set_size = [10, 20, 50, 100, 200, 500, 1000]
         self.val_patch_inference_steps = val_patch_inference_steps
+        self.val_patch_inference_downsample = val_patch_inference_downsample
         self.reset_val()
 
         self.distance = distance
@@ -121,11 +123,12 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
         parser.add_argument('--regularization', type=float, default=1e-4)
         parser.add_argument('--loss_name', type=str, default="CPC")
         parser.add_argument('--unet_type', type=str, default="gp")
-        parser.add_argument('--lr_milestones', nargs='*', default=[200])
+        parser.add_argument('--lr_milestones', nargs='*', default=[10000, 50000])
         parser.add_argument('--temperature', type=float, default=10)
         parser.add_argument('--temperature_decay', type=float, default=0.99)
         parser.add_argument('--pretrained_model', action='store_true')
         parser.add_argument('--val_patch_inference_steps', type=int, default=None)
+        parser.add_argument('--val_patch_inference_downsample', type=int, default=None)
         parser.add_argument('--resnet_size', type=int, default=18)
         return parser
 
@@ -158,22 +161,8 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
             out_tp_flip[..., :2] *= -1.
             out_tp_flip = out_tp_flip[..., shuffle_index]
 
-            # average embeddings
-
-            # if vis:
-            #     for bb in range(b):
-            #         embedding = [{"embedding": out.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"w"},
-            #                      {"embedding": out_tp.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"r"},
-            #                      {"embedding": out_flip.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"g"},
-            #                      {"embedding": out_tp_flip.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"b"}]
-            #         vis_anchor_embedding(embedding,
-            #             abs_coords[bb].detach().cpu().numpy(),
-            #             img[bb].detach().cpu().numpy(),
-            #             grad=None,
-            #             output_file=[f"{augmentation}_{self.global_step}_{bb}.jpg"])
-
-
             out = (out + out_tp + out_flip + out_tp_flip) / 4.
+
         elif augmentation=='rotation':
 
             with torch.no_grad():
@@ -191,25 +180,14 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
             # rotate output embeddings
             shuffle_index = [1, 0] + [_ for _ in range(2, out_r0.shape[-1])]
             out_r1 = out_r1[..., shuffle_index]
-            out_r1[..., 1] *= -1.
+            out_r1[..., 0] *= -1.
             out_r2[..., :2] *= -1.
             out_r3 = out_r3[..., shuffle_index]
-            out_r3[..., 0] *= -1.
+            out_r3[..., 1] *= -1.
 
-            # if vis:
-            #     for bb in range(b):
-            #         embedding = [{"embedding": out_r0.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"w"},
-            #                      {"embedding": out_r1.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"r"},
-            #                      {"embedding": out_r2.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"g"},
-            #                      {"embedding": out_r3.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"b"}]
-            #         vis_anchor_embedding(embedding,
-            #             abs_coords[bb].detach().cpu().numpy(),
-            #             img[bb].detach().cpu().numpy(),
-            #             grad=None,
-            #             output_file=[f"{augmentation}_{self.global_step}_{bb}.jpg"])
             out = (out_r0 + out_r1 + out_r2 + out_r3) / 4.
-
-        elif augmentation=='both':
+            
+        elif augmentation=='rotflip':
 
             with torch.no_grad():
                 inp_r0 = x.view(-1, c, pw, ph)
@@ -233,30 +211,15 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
             # rotate output embeddings
             shuffle_index = [1, 0] + [_ for _ in range(2, out_r0.shape[-1])]
             out_r1 = out_r1[..., shuffle_index]
-            out_r1[..., 1] *= -1.
+            out_r1[..., 0] *= -1.
             out_r2[..., :2] *= -1.
             out_r3 = out_r3[..., shuffle_index]
-            out_r3[..., 0] *= -1.
+            out_r3[..., 1] *= -1.
 
             out_tp = out_tp[..., shuffle_index]
             out_flip[..., :2] *= -1.
             out_tp_flip[..., :2] *= -1.
             out_tp_flip = out_tp_flip[..., shuffle_index]
-
-            # if vis:
-            #     for bb in range(b):
-            #         embedding = [{"embedding": out_r0.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"w"},
-            #                      {"embedding": out_r1.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"r"},
-            #                      {"embedding": out_r2.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"g"},
-            #                      {"embedding": out_r3.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"b"},
-            #                      {"embedding": out_tp.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"c"},
-            #                      {"embedding": out_flip.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"y"},
-            #                      {"embedding": out_tp_flip.view(b, p, self.out_channels)[bb].detach().cpu().numpy(), "color":"m"}]
-            #         vis_anchor_embedding(embedding,
-            #             abs_coords[bb].detach().cpu().numpy(),
-            #             img[bb].detach().cpu().numpy(),
-            #             grad=None,
-            #             output_file=[f"{augmentation}_{self.global_step}_{bb}.jpg"])
 
             # average embeddings
             out = (2 * out_r0 + out_r1 + out_r2 + out_r3 + out_tp + out_flip + out_tp_flip) / 8. 
@@ -360,12 +323,12 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
         #     self.anchor_loss.multiply_temperature(self.temperature_decay)
 
         self.log('anchor_loss', anchor_loss.detach(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('anchor_loss_temperature', self.anchor_loss.temperature, on_step=True,  on_epoch=True, prog_bar=True, logger=True)
+        self.log('anchor_loss_temperature', self.anchor_loss.temperature, on_step=True, prog_bar=True, logger=True)
 
         if self.regularization > 0.:
             reg_loss = self.regularization * embedding.norm(2, dim=-1).sum()
             loss = anchor_loss + reg_loss
-            self.log('reg_loss', reg_loss.detach(), on_step=True, on_epoch=True, prog_bar=True, logger=True)
+            self.log('reg_loss', reg_loss.detach(), on_step=True, prog_bar=True, logger=True)
         else:
             loss = anchor_loss
 
@@ -375,20 +338,46 @@ class SSLTrainer(pl.LightningModule, BuildFromArgparse):
         return {'loss': loss, 'log': tensorboard_logs}
 
     def validation_step(self, batch, batch_nb):
+
+        # set model to validation mode
+        self.model.eval()
         x, patches, abs_coords, patch_matches, mask, y = batch
 
-        with torch.no_grad():
+        import os, psutil
+        process = psutil.Process(os.getpid())
+        from inspect import currentframe, getframeinfo
 
+        with torch.no_grad():
+            print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
+            if self.val_patch_inference_downsample is not None:
+                patches = patches[:, ::self.val_patch_inference_downsample]
+                abs_coords = abs_coords[:, ::self.val_patch_inference_downsample]
+                patch_matches = patch_matches[:, ::self.val_patch_inference_downsample,
+                                                 ::self.val_patch_inference_downsample]
+            print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
             if self.val_patch_inference_steps is not None:
+                print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
+                print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
                 embedding = self.sliced_cpu_forward_patches(patches)
+                print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
                 abs_coords = abs_coords.cpu()
                 patch_matches = patch_matches.cpu()
+                print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
             else:
                 embedding = self.forward_patches(patches)
 
+            print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
+
             loss = self.anchor_loss(embedding, abs_coords, patch_matches)
+
+            print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
             
             self.log('val_loss', loss.detach(), on_step=True, on_epoch=True, prog_bar=False, logger=True)
+
+        print(getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno, process.memory_info().rss)
+
+        # set model back to training mode
+        self.model.train()
 
         return {'val_loss': loss}
 

scripts/prepare_sls_dsb2_training.py
@@ -22,11 +22,12 @@ def run(lr,
     args += f" --regularization {regularization}"
     args += f" --image_scale {image_scale}"
     args += f" --resnet_size 18"
-    args += f" --check_val_every_n_epoch 10"
+    args += f" --check_val_every_n_epoch 50"
     args += f" --max_epochs 200"
     args += f" --temperature {temperature}"
     args += f" --temperature_decay {temperature_decay}"
     args += f" --val_patch_inference_steps 2000"
+    args += f" --val_patch_inference_downsample 8"
     # args += f" --limit_val_batches 16"
     args += f" --gpu 1"
 
@@ -65,7 +66,7 @@ if __name__ == '__main__':
     #         for image_scale in [1.]:
     #           for anchor_radius in [2, 5, 10]:
 
-    base_lr = 4e-5
+    base_lr = 1e-6
     base_patchsize = 16
     base_regularization = 0.
     base_dist_factor = 10
@@ -73,7 +74,7 @@ if __name__ == '__main__':
     base_anchor_radius = 6
     base_temperature = 10.
     base_out_channels = 2
-    base_stride = 2
+    base_stride = 3
     base_patchdilation = 1
 
     temperature_decay = 1.
@@ -81,20 +82,20 @@ if __name__ == '__main__':
     temperature = 1.66
     positive_radius = 10
     # for temperature_decay in [math.pow(0.5, 1/halftime) for halftime in np.logspace(2, 6, base=10., num = 3)] + [1.]:
-    for image_scale in np.linspace(0.2, 2., num=4):
-        for regularization in np.logspace(-7, -1., base=10., num=3):
-            for temperature in [0.5, 1., 2.]:
+    for image_scale in [1., 2]:
+        for regularization in [1., 0.001, 0.00001]:#np.logspace(-7, -1., base=10., num=3):
+            for temperature in [1., 2., 4]:
                 # for positive_radius in np.linspace(5, 20, num=10):
-                    run(lr=base_lr,
-                        patchsize=base_patchsize,
-                        regularization=regularization,
-                        positive_radius=int(positive_radius),
-                        image_scale=image_scale,
-                        out_channels=base_out_channels,
-                        stride=base_stride,
-                        temperature=temperature,
-                        temperature_decay=temperature_decay)
-                    experiment_number += 1
+                run(lr=base_lr,
+                    patchsize=base_patchsize,
+                    regularization=regularization,
+                    positive_radius=int(positive_radius),
+                    image_scale=image_scale,
+                    out_channels=base_out_channels,
+                    stride=base_stride,
+                    temperature=temperature,
+                    temperature_decay=temperature_decay)
+                experiment_number += 1
 
 
     #     run(base_lr,