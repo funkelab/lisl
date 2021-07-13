1a59e0f3551cb3bf5b052d40fefc7e64b8dac95a
Steffen-Wolf <steffen.wolf.1989@gmail.com>
2021-07-13 19:18:33 -0400

    add upsampeling to the prediction script

--- lisl/predict/predict_ds_frame.py
+++ lisl/predict/predict_ds_frame.py
@@ -8,6 +8,8 @@ import gunpowder as gp
 import logging
 import lisl
 from lisl.pl.model import PatchedResnet
+from gunpowder.nodes.upsample import UpSample
+from gunpowder.nodes.downsample import DownSample
 
 logger = logging.getLogger(__name__)
 
@@ -29,7 +31,8 @@ def predict_frame(
         dataset_intermediate_key="interm_cooc_emb",
         model_input_tensor_name="patches",
         model_architecture="PatchedResnet",
-        num_workers=5):
+        num_workers=5,
+        upsample=1):
 
     # initialize model
     if model_architecture == "PatchedResnet":
@@ -46,7 +49,9 @@ def predict_frame(
     in_shape = gp.Coordinate(in_shape)
     out_shape = gp.Coordinate(out_shape)
     raw = gp.ArrayKey(f'RAW_{inference_frame}')
+    raw_us = gp.ArrayKey(f'RAW_UPSAMPLED_{inference_frame}')
     prediction = gp.ArrayKey(f'PREDICTION_{inference_frame}')
+    prediction_ds = gp.ArrayKey(f'PREDICTION_DS_{inference_frame}')
     intermediate_prediction = gp.ArrayKey(f'ITERM_{inference_frame}')
 
     ds_key = f'{inference_frame}/{dataset_raw_key}'
@@ -56,7 +61,7 @@ def predict_frame(
     # build pipeline
     zsource = gp.ZarrSource(input_dataset_file,
                             {raw: ds_key},
-                            {raw: gp.ArraySpec(interpolatable=True, voxel_size=(1, 1))})
+                            {raw: gp.ArraySpec(interpolatable=True, voxel_size=(upsample, upsample))})
 
     pipeline = zsource
     with gp.build(zsource):
@@ -74,20 +79,31 @@ def predict_frame(
         pred_dict[intermediate_layer] = intermediate_prediction
         pred_spec[intermediate_prediction] = gp.ArraySpec(roi=raw_roi)
 
+    if upsample > 1:
+        pipeline += UpSample(raw, upsample, raw_us)
+        netin = raw_us
+    else:
+        netin = raw
+
     pipeline += gp.torch.Predict(
         model,
-        inputs={model_input_tensor_name: raw},
+        inputs={model_input_tensor_name: netin},
         outputs=pred_dict,
         array_specs=pred_spec,
         checkpoint=model_checkpoint,
         spawn_subprocess=True
     )
 
+    if upsample > 1:
+        pipeline += DownSample(prediction, upsample, prediction_ds)
+    else:
+        prediction_ds = prediction
+
     request = gp.BatchRequest()
     request.add(raw, in_shape)
-    request.add(prediction, out_shape)
+    request.add(prediction_ds, out_shape)
 
-    zarr_dict = {prediction: out_key}
+    zarr_dict = {prediction_ds: out_key}
     if intermediate_layer is not None:
         zarr_dict[intermediate_prediction] = interm_key
         request.add(intermediate_prediction, out_shape)
@@ -99,7 +115,7 @@ def predict_frame(
         compression_type='gzip')
 
     total_request = gp.BatchRequest()
-    total_request[prediction] = gp.ArraySpec(roi=raw_roi)
+    total_request[prediction_ds] = gp.ArraySpec(roi=raw_roi)
     if intermediate_layer is not None:
         total_request[intermediate_prediction] = gp.ArraySpec(roi=raw_roi)
     with gp.build(pipeline):
@@ -117,7 +133,8 @@ if __name__ == "__main__":
     parser.add_argument('--out_dir')
     parser.add_argument('--out_filename')
     parser.add_argument('--inference_frame')
-    parser.add_argument('--intermediate_layer', type=int)
+    parser.add_argument('--intermediate_layer', type=int, default=None)
+    parser.add_argument('--upsample', type=int, default=1)
     parser.add_argument('--dataset_raw_key', default="train/raw")
     parser.add_argument('--dataset_prediction_key', default="train/prediction")
     parser.add_argument('--dataset_intermediate_key', default="interm_cooc_emb")
@@ -143,4 +160,5 @@ if __name__ == "__main__":
         dataset_prediction_key=options.dataset_prediction_key,
         model_architecture=options.model_architecture,
         dataset_intermediate_key=options.dataset_intermediate_key,
-        model_input_tensor_name=options.model_input_tensor_name)
+        model_input_tensor_name=options.model_input_tensor_name,
+        upsample=options.upsample)
